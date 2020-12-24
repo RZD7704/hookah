96e94a84d0a63eedc542f124ea700bc4c1c98b46
Artem <artemius2014poltava@gmail.com>
2020-12-24 18:34:47 +0200

    Add header

---
+++ #src/js/script.js
@@ -0,0 +1,7 @@
+// @@include('alert.js')
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
