8b7b79ddd1ca4e8a15ee213d95e5e60a51006e28
Artem <artemius2014poltava@gmail.com>
2020-12-30 00:16:00 +0200

    Add new page

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,65 +4,79 @@ $(document).ready(function() {
 		$('.header__burger,.header-menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-});
-
-
-
-function resizeScrenn() {
-	if ($(window).width() < 992) {
-		var swiper1 = new Swiper('.section-buy-slider', {
-			slidesPerView: 1,
-			spaceBetween: 30,
-			breakpoints: {
-				// when window width is <= 499px
-				320: {
-					slidesPerView: 1,
-					spaceBetweenSlides: 15
+	
+	function resizeScrenn() {
+		if ($(window).width() < 992) {
+			var swiper1 = new Swiper('.section-buy-slider', {
+				slidesPerView: 1,
+				spaceBetween: 30,
+				breakpoints: {
+					// when window width is <= 499px
+					320: {
+						slidesPerView: 1,
+						spaceBetweenSlides: 15
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetweenSlides: 20
+					},
+					// when window width is <= 999px
+					992: {
+						slidesPerView: 3,
+						spaceBetweenSlides: 20
+					}
 				},
-				768: {
-					slidesPerView: 2,
-					spaceBetweenSlides: 20
+				navigation: {
+					nextEl: '.general-pagination__btn_next',
+					prevEl: '.general-pagination__btn_prev',
 				},
-				// when window width is <= 999px
-				992: {
-					slidesPerView: 3,
-					spaceBetweenSlides: 20
-				}
-			},
-			navigation: {
-				nextEl: '.general-pagination__btn_next',
-				prevEl: '.general-pagination__btn_prev',
-			},
-		});
+			});
 
-		var swiper2 = new Swiper('.section-shop-slider', {
-			slidesPerView: 1,
-			spaceBetween: 30,
-			breakpoints: {
-				// when window width is <= 499px
-				320: {
-					slidesPerView: 1,
-					spaceBetweenSlides: 15
+			var swiper2 = new Swiper('.section-shop-slider', {
+				slidesPerView: 1,
+				spaceBetween: 30,
+				breakpoints: {
+					// when window width is <= 499px
+					320: {
+						slidesPerView: 1,
+						spaceBetweenSlides: 15
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetweenSlides: 20
+					},
+					// when window width is <= 999px
+					992: {
+						slidesPerView: 3,
+						spaceBetweenSlides: 20
+					}
 				},
-				768: {
-					slidesPerView: 2,
-					spaceBetweenSlides: 20
+				navigation: {
+					nextEl: '.general-pagination__btn_next',
+					prevEl: '.general-pagination__btn_prev',
 				},
-				// when window width is <= 999px
-				992: {
-					slidesPerView: 3,
-					spaceBetweenSlides: 20
-				}
-			},
-			navigation: {
-				nextEl: '.general-pagination__btn_next',
-				prevEl: '.general-pagination__btn_prev',
-			},
-		});
+			});
+		}
 	}
-  }
-resizeScrenn();
-
-$(window).resize(function () {
 	resizeScrenn();
+
+	$(window).resize(function () {
+		resizeScrenn();
+	});
+
+	var swiper = new Swiper('.section-partners-slider', {
+		navigation: {
+			nextEl: '.general-pagination__btn_next',
+			prevEl: '.general-pagination__btn_prev',
+		},
+	});
+
+	var swiper2 = new Swiper('.section-everything-partners-slider', {
+		navigation: {
+			nextEl: '.general-pagination__btn_next',
+			prevEl: '.general-pagination__btn_prev',
+		},
+	});
 });
+
+
