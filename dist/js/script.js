853e881dbd2657e2fa28f8d488512393c32b7bf8
Artem <artemius2014poltava@gmail.com>
2021-01-21 18:38:45 +0200

    Add last changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -78,6 +78,13 @@ $(document).ready(function() {
 		},
 	});
 
+	var swiper3 = new Swiper('.section-everything-partners-slider', {
+		navigation: {
+			nextEl: '.general-pagination__btn_next',
+			prevEl: '.general-pagination__btn_prev',
+		},
+	});
+
 	$('.archive-filter').on('click', '.archive-filter__headline', function() {
 		$(this).toggleClass('active').siblings('.archive-filter__list').slideToggle();
 	});
@@ -384,3 +391,208 @@ function creatingEditedFields (valueArr) {
 	`;
 	addAdvertisingEditing();
 }
+
+let map;
+
+function initMap() {
+	let map, coords, styles, marker, info, content;
+
+	coords = {
+		lat: -24.548298,
+		lng: 133.200039
+	};
+
+	content = '<h1 class="info-title">I\'m here</h1>';
+
+	styles = [{
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#ebe3cd"
+			}]
+		},
+		{
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#523735"
+			}]
+		},
+		{
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"color": "#f5f1e6"
+			}]
+		},
+		{
+			"featureType": "administrative",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#c9b2a6"
+			}]
+		},
+		{
+			"featureType": "administrative.land_parcel",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#dcd2be"
+			}]
+		},
+		{
+			"featureType": "administrative.land_parcel",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#ae9e90"
+			}]
+		},
+		{
+			"featureType": "landscape.natural",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#dfd2ae"
+			}]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#dfd2ae"
+			}]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#93817c"
+			}]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#a5b076"
+			}]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#447530"
+			}]
+		},
+		{
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#f5f1e6"
+			}]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#fdfcf8"
+			}]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#f8c967"
+			}]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#e9bc62"
+			}]
+		},
+		{
+			"featureType": "road.highway.controlled_access",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#e98d58"
+			}]
+		},
+		{
+			"featureType": "road.highway.controlled_access",
+			"elementType": "geometry.stroke",
+			"stylers": [{
+				"color": "#db8555"
+			}]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#806b63"
+			}]
+		},
+		{
+			"featureType": "transit.line",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#dfd2ae"
+			}]
+		},
+		{
+			"featureType": "transit.line",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#8f7d77"
+			}]
+		},
+		{
+			"featureType": "transit.line",
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"color": "#ebe3cd"
+			}]
+		},
+		{
+			"featureType": "transit.station",
+			"elementType": "geometry",
+			"stylers": [{
+				"color": "#dfd2ae"
+			}]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [{
+				"color": "#b9d3c2"
+			}]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#92998d"
+			}]
+		}
+	];
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: coords,
+		zoom: 4,
+		styles: styles,
+		disableDefaultUI: true,
+		zoomControl: true,
+		streetViewControl: true,
+	});
+
+	marker = new google.maps.Marker({
+		position: coords,
+		map: map,
+		// icon: 'images/marker.png',
+		draggable: true
+	});
+
+	info = new google.maps.InfoWindow({
+		content: content
+	});
+
+	marker.addListener('click', function () {
+		info.open(map, marker);
+	});
+}
+
